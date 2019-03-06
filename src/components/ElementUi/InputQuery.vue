b3a17906931fc48a0ceba7bf619098a3cccb1be9
zhangbinru <569488212@qq.com>
2019-03-06 12:34:35 +0800

    筛选验证完成

--- src/components/ElementUi/InputQuery.vue
+++ src/components/ElementUi/InputQuery.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="check2">
-    <el-form :inline="true"  class="demo-form-inline">
-      <template v-for="(tableTitle,index) in data" :model="data[index]">
+    <el-form :model="data_model" ref="data_model" :inline="true" class="demo-form-inline">
+      <template v-for="(tableTitle,index) in data">
         <!-- 0: str,  1: int, 2:date 3: status 4.deadline-->
         <!-- 带有搜索建议的输入框    字符型-->
         <el-form-item
           :label="tableTitle.headName"
           v-if="tableTitle.inputType === 0"
           v-show="isShow(tableTitle.id)"
-          prop="0"
+          :prop="tableTitle.topType"
+          :rules="rules.str"
         >
           <el-autocomplete
             class="inline-input"
-            v-model="data[index]['_value']"
+            v-model="data_model[tableTitle.topType]"
             :fetch-suggestions="getQuerySuggestions"
             :placeholder="'请输入 ' + tableTitle.headName"
             @focus="curr_query_field=tableTitle.topType"
@@ -24,12 +25,12 @@
           :label="tableTitle.headName"
           v-if="tableTitle.inputType === 1"
           v-show="isShow(tableTitle.id)"
-          prop="_value"
-          :rules="ruls.number"
+          :prop="tableTitle.topType"
+          :rules="rules.number"
         >
           <el-autocomplete
             class="inline-input"
-            v-model="data[index]['_value']"
+            v-model="data_model[tableTitle.topType]"
             :fetch-suggestions="getQuerySuggestions"
             :placeholder="'请输入 ' + tableTitle.headName"
             @focus="curr_query_field=tableTitle.topType"
@@ -41,23 +42,25 @@
           :label="tableTitle.headName"
           v-if="tableTitle.inputType === 2"
           v-show="isShow(tableTitle.id)"
+          :prop="tableTitle.topType"
+          :rules="rules.date"
         >
           <el-checkbox
             v-if="tableTitle.topType == 'pwdValidityPeriod'"
-            v-model="data[index]['pwdAlways']"
+            v-model="data_model['pwdAlways']"
             @change="isValid($event,data[index])"
           >始终有效</el-checkbox>
 
           <el-checkbox
             v-if="tableTitle.topType == 'userExpirationDate'"
-            v-model="data[index]['uAlways']"
+            v-model="data_model['uAlways']"
             @change="isValid($event,data[index])"
           >始终有效</el-checkbox>
 
           <el-date-picker
             v-show="tableTitle._isShow"
             value-format="timestamp"
-            v-model="data[index]['_value']"
+            v-model="data_model[tableTitle.topType]"
             type="date"
             placeholder="选择日期"
           ></el-date-picker>
@@ -84,8 +87,10 @@
           :label="tableTitle.headName"
           v-if="tableTitle.inputType === 3"
           v-show="isShow(tableTitle.id)"
+          :prop="tableTitle.topType"
+          :rules="rules.status"
         >
-          <el-select v-model="data[index]['_value']" value placeholder="请输入账号状态">
+          <el-select v-model="data_model[tableTitle.topType]" value placeholder="请输入账号状态">
             <el-option
               v-for="(item,index) in tableTitle.statusOptions"
               :key="index"
@@ -100,7 +105,7 @@
 </template>
 
 <script>
-import {isNumber} from '../../utils/verify.js';
+import { isNumber } from "../../utils/verify.js";
 import PubSub from "pubsub-js";
 import { unique } from "../../utils/Arrays";
 
@@ -112,30 +117,15 @@ export default {
     querySuggestionsConfig: Object //模糊查询的配置对象
   },
   data() {
-    const isNumber = (rule, value, cb) => {
-  let re = /^[0-9]+.?[0-9]*$/; //判断字符串是否为数字 //判断正整数 /^[1-9]+[0-9]*]*$/ 
-  let val = Number(value);
-  
-  if (!re.test(val)) {
-    cb(new Error('请输入数字'));
-  } else {
-    cb();
-  }
-}
     return {
       data: [],
+      data_model: {},
       curr_query_field: "", //当前查询搜索建议的字段
       rules: {
-        0: [
-          { required: true, message: "请输入字符", trigger: "blur" },
-        ],
-        number: [
-           {validator:isNumber, trigger: "blur" },
-        ],
-        name: [
-          { required: true, message: "请输入活动名称", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
-        ]
+        str: [{ required: false, message: "请输入字符", trigger: "change" }],
+        number: [{ required: false, validator: isNumber, trigger: "change" }],
+        date: [{ required: false, message: "请输入日期", trigger: "change" }],
+        status: [{ required: false, message: "请选择状态", trigger: "change" }]
       }
     };
   },
@@ -144,23 +134,29 @@ export default {
       this.updateTableTitle();
     },
     //改变数据后向上传递
-    data: {
+    data_model: {
       handler() {
-        let obj = {};
-        this.data.forEach(item => {
-          obj[item.topType] = item._value ? item._value : "";
-          if (item.hasOwnProperty("uAlways")) {
-            obj.uAlways = item.uAlways;
-          }
-          if (item.hasOwnProperty("pwdAlways")) {
-            obj.pwdAlways = item.pwdAlways;
-          }
-        });
-        this.$emit("changeQuery", [obj]);
-        console.log(obj);
+        this.changeQuery();
       },
       deep: true
     },
+    // data: {
+    //   handler() {
+    //     let obj = {};
+    //     this.data.forEach(item => {
+    //       obj[item.topType] = item._value ? item._value : "";
+    //       if (item.hasOwnProperty("uAlways")) {
+    //         obj.uAlways = item.uAlways;
+    //       }
+    //       if (item.hasOwnProperty("pwdAlways")) {
+    //         obj.pwdAlways = item.pwdAlways;
+    //       }
+    //     });
+    //     this.$emit("changeQuery", [obj]);
+    //     console.log(obj);
+    //   },
+    //   deep: true
+    // },
     //根据勾选顺序对展示的查询列表进行排序
     selectedIds(val, old) {
       const self = this;
@@ -178,6 +174,7 @@ export default {
       this.data = [...arr, ...data];
 
       //多选select移除tag时,初始化值
+
       this.data.forEach(item => {
         if (!val.includes(item.id)) {
           this.initValue(item);
@@ -194,6 +191,20 @@ export default {
       data.currentPage = 1;
       return data;
     }
+    //需要绑定向上回传的数据
+    // data_model() {
+    //   let obj = {};
+    //   this.data.forEach(item => {
+    //     obj[item.topType] = item._value ? item._value : "";
+    //     if (item.hasOwnProperty("uAlways")) {
+    //       obj.uAlways = item.uAlways;
+    //     }
+    //     if (item.hasOwnProperty("pwdAlways")) {
+    //       obj.pwdAlways = item.pwdAlways;
+    //     }
+    //   });
+    //   return obj;
+    // }
   },
   methods: {
     //根据父组件传递的数据生成对应的查询列表数据
@@ -208,28 +219,49 @@ export default {
     },
     //初始化数据
     initValue(tableTitle) {
-      tableTitle._value = "";
-      if (tableTitle.inputType == 2) {
-        switch (tableTitle.topType) {
-          //用户有效期
-          case "":
-            tableTitle.uAlways = false;
-            break;
-          //密码有效期
-          case "":
-            tableTitle.pwdAlways = false;
-            break;
+      if (tableTitle) {
+        let key = tableTitle.topType;
+        // this.data_model[key] = '';
+        // tableTitle._value = "";
+        this.$set(this.data_model, key, "");
+        if (tableTitle.inputType == 2) {
+          switch (tableTitle.topType) {
+            //用户有效期
+            case "userExpirationDate":
+              this.data_model.uAlways = false;
+              break;
+            //密码有效期
+            case "pwdValidityPeriod":
+              this.data_model.pwdAlways = false;
+              break;
+          }
+          tableTitle._isShow = true;
         }
-        tableTitle._isShow = true;
       }
     },
     //查询 搜索建议下拉列表
     async getQuerySuggestions(queryString, cb) {
-      let query = { ...this._querySuggestionsConfig };
+      console.log( this.$refs['data_model']);
+      console.log(1111);
+      
+      this.$refs['data_model'].validate(valid => {
+        console.log(valid);
+        
+        if (valid) {
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+      let query = JSON.parse(JSON.stringify(this._querySuggestionsConfig));
+      // let query = { ...this._querySuggestionsConfig };
+
+      
       let field = this.curr_query_field;
       query[field] = queryString;
+      
       let res = await this.querySuggestionsMethod(query);
-      if (!res.data.dataList.length) return;
+
       let data = res.data.dataList.map(item => {
         return item[field];
       });
@@ -247,8 +279,14 @@ export default {
     isValid(val, data) {
       data._isShow = !val;
       if (val) {
-        data._value = "";
+        this.data_model[data.topType] = "";
       }
+      this.changeQuery();
+    },
+    //向上回传数据
+    changeQuery() {
+      this.$emit("changeQuery", [this.data_model]);
+      console.log(this.data_model);
     }
   },
   created() {
