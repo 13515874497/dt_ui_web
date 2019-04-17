e7f9e835df76cd135bc684886c09ccc39588705f
Jasmine <1710075797@qq.com>
2019-04-17 10:18:46 +0800

    huilv

--- src/views/Basic_Data/Public_Data/Exchange-rate.vue
+++ src/views/Basic_Data/Public_Data/Exchange-rate.vue
@@ -9,15 +9,7 @@ export default {
   data(){
     return{
       showQuery:true,//是否显示上方查询功能
-      primaryKey:"currencyId",//修改删除的时候需要一个key
-      rule:{
-        //某些字段的验证规则
-        companyShortCode:[{
-          validator:lessThan2, //该字段的自定义验证规则
-          trigger:"change"
-
-        }]
-      }
+      primaryKey:"exchangeRateId",//修改删除的时候需要一个key
     }
   },
   methods: {
