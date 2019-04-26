254419a560c751ea5e2c43bfdc4e2c64be73b42b
Jasmine <1710075797@qq.com>
2019-04-26 17:27:24 +0800

    ...

--- src/components/FileUpload/FilesUpload.vue
+++ src/components/FileUpload/FilesUpload.vue
@@ -31,14 +31,6 @@
           ></el-option>
         </el-select>
 
-<<<<<<< HEAD
-        <el-date-picker
-          v-model="value2"
-          type="month"
-          placeholder="选择月"
-          :picker-options="pickerOptions">
-        </el-date-picker>
-=======
         <el-select
           placeholder="请选择"
           v-if="select_site.render.length && select_area.model"
@@ -53,8 +45,7 @@
           ></el-option>
         </el-select>
 
-        <el-date-picker v-model="value2" type="month" placeholder="选择月"></el-date-picker>
->>>>>>> f7e2a8a2fb07c56910f1d7fd09b73c71a3fbca7d
+        <el-date-picker v-model="value2"  type="month" placeholder="选择月" :picker-options="pickerOptions" v-if="flag"></el-date-picker>
 
         <el-button type="primary" class="el-btn">设为默认按钮</el-button>
       </div>
@@ -208,11 +199,12 @@ export default {
   data() {
     return {
       value2: "",
-      pickerOptions: {
+      pickerOptions: {     
         disabledDate(time) {
-          return time.getTime() > Date.now();
+          return time.getTime() > Date.now()
         }
       },
+      flag:true,
       page: {
         name: this.$route.params.name,
         id: +this.$route.params.id
@@ -264,7 +256,8 @@ export default {
         page_sizes: [5, 10, 15, 20, 25]
       },
       notifys: [], //当前页面的所有提示信息
-      financialIntroduction: [85] //财务导入的id
+      financialIntroduction: [85] ,//财务导入的id,
+      businessReport:[108],//业务报告id
     };
   },
   computed: {
@@ -405,16 +398,36 @@ export default {
     },
 
     async getDate() {
+      if(this.businessReport.includes(this.page.id)){
+        this.flag = false;
+      }
       if (this.financialIntroduction.includes(this.page.id)) {
         let res = await getCheckoutDate({ menuId: "111" });
         console.log(res);  //财务导入
-        if(res.code === 200){
-          this.value2 = res.data 
-        }
+       if(res.code === 200){
+         this.value2 = res.data.split(":")[0];  
+        let time = res.data.split(':')
+        let startTime = time[0]
+        let endTime = time[1]
+        let startTime_ym = startTime.split('-')
+        let startTime_y = startTime_ym[0]
+        let startTime_m = startTime_ym[1]
+        var date = (new Date(startTime_y,startTime_m )) -1; 
+        console.log(date)
+        
+        let endTime_ym = endTime.split('-')
+        let endTime_y = endTime_ym[0]
+        let endTime_m = endTime_ym[1]
+        var date1 = (new Date(endTime_y,endTime_m)) -1; 
+        console.log(date1)
+       }
         
       } else {
         let res = await getCheckoutDate({ menuId: "102" });
         console.log(res); //运营导入
+        if(res.code === 200){
+         this.value2 = res.data.split(":")[0]
+       }
       }
     },
 
@@ -891,12 +904,6 @@ export default {
     this.getRadioList();
     this.initOperateBtn();
     this.getDate();
-<<<<<<< HEAD
-  },
-  mounted() {
-  
-=======
->>>>>>> f7e2a8a2fb07c56910f1d7fd09b73c71a3fbca7d
   },
   mounted() {},
   activated() {
