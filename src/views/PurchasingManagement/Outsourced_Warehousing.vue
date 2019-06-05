c424821b85153225b3a0a2586310689a69e0c325
Jasmine <1710075797@qq.com>
2019-06-05 13:21:18 +0800

    更新

--- src/views/PurchasingManagement/Outsourced_Warehousing.vue
+++ src/views/PurchasingManagement/Outsourced_Warehousing.vue
@@ -1,9 +1,14 @@
 <script>
 //外购入库
-import { getIcBillStock, saveIcBillStock, delIcBillStock, uplIcBillStock } from "@/api";
 import {
- supplierId,//供应商
- findListWar //仓库
+  getIcBillStock,
+  saveIcBillStock,
+  delIcBillStock,
+  uplIcBillStock
+} from "@/api";
+import {
+  supplierId, //供应商
+  findListWar //仓库
 } from "@/components/ElementUi/Form/customField";
 import MxTable2 from "@/components/Mixins/MxTable2";
 import { isRepetArr } from "@/utils/Arrays";
@@ -12,29 +17,28 @@ export default {
   mixins: [MxTable2],
   data() {
     return {
-      nameKey:'no', //点击删除，失败的话会有提示，这个是需要传入的key
-      primaryKey: "sbId",             //后台返回的
-      primaryKey_child: 'sbeId',       //后台返回的
-      queryKey: 'purchaseIcBillStockEntry', //后台返回
+      nameKey: "no", //点击删除，失败的话会有提示，这个是需要传入的key
+      primaryKey: "sbId", //后台返回的
+      primaryKey_child: "sbeId", //后台返回的
+      queryKey: "purchaseIcBillStockEntry", //后台返回
       customField: [
         supplierId,
-       
-          //要验证的字段        
+
+        //要验证的字段
         {
           topType: "date",
           required: true
-        },        
+        },
         {
           topType: "no",
           required: true
-        },
+        }
       ],
-     
+
       customField_table: [
-         findListWar,
-       //  自定义字段表
-        { 
-          
+        findListWar,
+        //  自定义字段表
+        {
           // inputType: 3,
           // topType: "sku",
           // bindKey: "skuId",
@@ -51,15 +55,16 @@ export default {
       editable_field: [
         //表格中哪些字段可以被编辑
         "eRemark",
-        "quantity",
+        "quantity"
       ],
       parentKey: "purchaseIcBillStock", // 点击新增、修改的时候传给后台的 父key的名字 (后台让传的参数)
       subField: {
-        "1": { //1代表  第一个二级子字段  2代表第二个子字段  1-1代表第1个2级子字段的第1个3级子字段(暂时不考虑3级子字段)
+        "1": {
+          //1代表  第一个二级子字段  2代表第二个子字段  1-1代表第1个2级子字段的第1个3级子字段(暂时不考虑3级子字段)
           //radio选项  和  点击新增、修改的时候传给后台的key
           name: "外购入库单",
           key_submit: "purchaseIcBillStockEntry", //传给后台的子key(后台给的)
-          key_get: 'purchaseIcBillStockEntryList' //获取是从后台返回数据拿出来
+          key_get: "purchaseIcBillStockEntryList" //获取是从后台返回数据拿出来
         }
       }
     };
@@ -78,35 +83,55 @@ export default {
       //   })
       //   fba.required = true;
       // }
-    },
-
+    }
   },
   methods: {
+   initTableOperateList() {
+      let self = this;
+      this.tableOperateList = [
+        {
+          type: "primary",
+          icon: "",
+          label: "发票",
+          fn(row, mull) {
+            self.addRow(row, mull);
+          },
+          isShow(row,mull){
+            console.log(row)
+            console.log(mull)
+            return mull.length
+          }
+
+        }
+      ];   
+    },
+
+
 
     queryPage(data) {
       return getIcBillStock(data); //查询页面的接口
     },
 
-
     ajax_add(data) {
-      data.mid = 295 //后台需要传入的参数mid就是当前页面的id
-      delete data.entry[0]._reciveWarehouseId
+      data.mid = 295; //后台需要传入的参数mid就是当前页面的id
+      delete data.entry[0]._reciveWarehouseId;
       return saveIcBillStock(data); //新增的接口
     },
 
     ajax_update(data) {
-      delete data.entry[0]._reciveWarehouseId //删除级联选择器中返回给后台的不需要的数据
+      delete data.entry[0]._reciveWarehouseId; //删除级联选择器中返回给后台的不需要的数据
       return uplIcBillStock(data); //修改
     },
-     ajax_remove(data) {
+    ajax_remove(data) {
       return delIcBillStock(data); //删除
-    },
-   
+    }
   },
   beforeCreate() {
     // transportTypeName.hideChild = true;
   },
-  async created() {}
+  async created() {
+    this.initTableOperateList();
+  }
 };
 </script>
 
